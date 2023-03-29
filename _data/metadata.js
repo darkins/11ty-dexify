5ac6734faf13f83f3b85221624fd73ce5cca3785
darkins <dec.arkins@gmail.com>
2023-03-29 18:10:38 +0100

    adjusting colors

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,5 +1,5 @@
 module.exports = {
-	"title": "Dexify",
+	"title": "Citizens' Assembly Project",
 	"url": "https://dexark.ie",
 	"language": "en",
 	"description": "This is my 11ty and Netlify starter project.",
